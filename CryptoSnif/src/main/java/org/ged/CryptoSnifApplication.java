58362001b8fa5311bbba57f5d72232f2203b1431
pippuzzo <filippo.aquino@gmail.com>
2020-11-14 01:20:47 +0100

    test commit

---
+++ CryptoSnif/src/main/java/org/ged/CryptoSnifApplication.java
@@ -0,0 +1,13 @@
+package org.ged;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoSnifApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoSnifApplication.class, args);
+	}
+
+}
