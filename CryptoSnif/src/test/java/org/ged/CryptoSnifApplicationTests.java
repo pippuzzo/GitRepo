c5ca67f339588ddebfdcf5e7ea1c344a41661e86
pippuzzo <filippo.aquino@gmail.com>
2020-11-14 01:50:36 +0100

    commit

---
+++ CryptoSnif/src/test/java/org/ged/CryptoSnifApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ged;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoSnifApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
